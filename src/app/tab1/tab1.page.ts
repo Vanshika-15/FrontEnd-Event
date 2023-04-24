2aa59bac44e19c213f941df90862aa7a56ed5c3f
Upneet <kaurupneet429@gmail.com>
2023-04-24 11:19:44 +0530

    commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { HomeService } from '../services/home.service';
-import { Session } from 'inspector';
+
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -20,12 +20,12 @@ export class Tab1Page implements OnInit{
     })
   }
   getSessionDetails(){
-   
+  
   this.data2.getSessionDetails().subscribe(response => {
     this.sessionDetails = response;
-    if(this.test.topic==this.sessionDetails.topic){
+   
     console.log(this.sessionDetails);
-  }})
+  })
 };
 }
 
